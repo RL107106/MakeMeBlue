781021818adf36c060bbcf32c98428774f4b7ace
NATHAN GEHRKE <rl107106@rlas-macair-22.local>
2018-12-11 11:27:21 -0600

    Initial Commit

---
+++ MakeMeBlue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MakeMeBlue
+//
+//  Created by NATHAN GEHRKE on 8/31/18.
+//  Copyright © 2018 Nathan Gehrke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var makeMeBlueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       makeMeBlueLabel.textColor = .red
+       makeMeBlueLabel.text = "Jacket"
+        
+    
+        
+    }
+
+
+
+
+}
+
